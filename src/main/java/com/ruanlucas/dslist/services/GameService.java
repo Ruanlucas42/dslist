4b0c11e6a97869c158d024931c48f316a220cabb
Ruan Lucas <rulucasilva13@gmail.com>
2025-01-22 17:02:45 -0300

    SQL, projection, get games by list

--- src/main/java/com/ruanlucas/dslist/services/GameService.java
+++ src/main/java/com/ruanlucas/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ruanlucas.dslist.dto.GameDTO;
 import com.ruanlucas.dslist.dto.GameMinDTO;
 import com.ruanlucas.dslist.entities.Game;
+import com.ruanlucas.dslist.projections.GameMinProjection;
 import com.ruanlucas.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
